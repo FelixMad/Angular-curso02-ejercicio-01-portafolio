25189406985326830c3a9dd12c4200450a1a43b8
Felix Garrido <felix.garrido@gmail.com>
2019-08-19 13:10:01 +0100

    Creación de la pagina portafolio como parte del contenido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './share/header/header.component';
 import { FooterComponent } from './share/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
