f4569860ddc647bb5c4f11e9aa82aec6bd696d8e
Felix Garrido <felix.garrido@gmail.com>
2019-08-22 18:28:26 +0100

    Creación de la página de Busqueda y funcionalidad de esta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { PortafolioComponent } from '../app/pages/portafolio/portafolio.component';
 import { ItemComponent } from '../app/pages/item/item.component';
 import { AboutComponent } from '../app/pages/about/about.component';
-
+import { SearchComponent } from '../app/pages/search/search.component';
 
 
 const routes: Routes = [
   { path: 'home', component: PortafolioComponent },
   { path: 'item/:id', component: ItemComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'search/:termino', component: SearchComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
--- src/app/providers/productos.service.ts
+++ src/app/providers/productos.service.ts
@@ -11,6 +11,7 @@ export class ProductosService {
 
   cargando = true;
   productos: ProductosPagina[] = [];
+  productosFiltrado: ProductosPagina[] = [];
 
   constructor( private http: HttpClient ) {
     this.cargarProductos();
@@ -18,18 +19,56 @@ export class ProductosService {
 
   private cargarProductos(){
 
-    this.http.get('https://portfolio-c84ef.firebaseio.com/productos_idx.json')
+    return new Promise( (resolve, reject) => {
+      this.http.get('https://portfolio-c84ef.firebaseio.com/productos_idx.json')
       .subscribe( (resp: ProductosPagina[]) => {
 
         this.productos = resp;
         this.cargando = false;
+        resolve();
 
-      } )
+      });
+    })
+
   }
 
   public getProducto( id: string ){
     return this.http.get(`https://portfolio-c84ef.firebaseio.com/productos/${id}.json`);
   }
 
+  public buscarProducto( termino: string ){
 
+    if( this.productos.length === 0 ){
+
+      this.cargarProductos().then( () => {
+
+        this.filtrarProductos( termino );
+      });
+
+    }else{
+      this.filtrarProductos( termino );
+    }
+
+
+  }
+
+  private filtrarProductos( termino: string ) {
+
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0  ) {
+        this.productosFiltrado.push( prod );
+      }
+
+    });
+
+
+  }
 }
+
