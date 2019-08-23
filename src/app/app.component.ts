c502b69aaae6a12b0b14d65e0373e9fd9e7e377e
Felix Garrido <felix.garrido@gmail.com>
2019-08-23 10:20:30 +0100

    Limpieza de codigo innecesario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { ProductosService } from '../app/providers/productos.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: []
 })
 export class AppComponent {
 
